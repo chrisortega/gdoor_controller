e2728b72a2fd7034aa5c5617028a85cb685672a2
ivan <christian-ortega@outlook.com>
2023-04-18 21:13:27 -0900

    adding templates and static

--- app/app.py
+++ app/app.py
@@ -1,16 +1,20 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import json
-
+from door import GdoorControl
 
 app = Flask(__name__)
-resources = {r"/api/*": {"origins": "*"}}
-app.config["CORS_HEADERS"] = "Content-Type"
-app.config['JSON_SORT_KEYS'] = False
 
 
 @app.route('/')
 def home():
+    return render_template('index.html', test = 1)
+
+@app.route('/api/toggle')
+def api():
+    gdc = GdoorControl()
+    gdc.toggle_pin(17)
+    gdc.toggle_pin(22)
     return jsonify({"Message":"This is your flask app with docker"})
 
 if __name__ == "__main__":
-    app.run(debug = True, host='0.0.0.0', port=80)
+    app.run(debug = True, host='0.0.0.0', port=5000)
