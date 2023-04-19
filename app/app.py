b4cbe8a5609017deceecff23e8eec7e80f89236a
ivan <christian-ortega@outlook.com>
2023-04-18 20:19:22 -0900

    adding docker

---
+++ app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+import json
+
+
+app = Flask(__name__)
+resources = {r"/api/*": {"origins": "*"}}
+app.config["CORS_HEADERS"] = "Content-Type"
+app.config['JSON_SORT_KEYS'] = False
+
+
+@app.route('/')
+def home():
+    return jsonify({"Message":"This is your flask app with docker"})
+
+if __name__ == "__main__":
+    app.run(debug = True, host='0.0.0.0', port=80)
